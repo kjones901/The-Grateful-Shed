271424115ad11e8b33ecd65bbbdccfcf54b8172b
Kyle Jones <kyle@Kyles-MacBook-Pro.local>
2023-11-01 13:17:57 -0600

    updated pictures in gallery

--- src/Pages/Gallery/index.jsx
+++ src/Pages/Gallery/index.jsx
@@ -10,43 +10,50 @@ import Before4 from "../../assets/Gbefore4.jpeg";
 import After4 from "../../assets/Gafter4.jpeg";
 import Before5 from "../../assets/Gbefore5.jpeg";
 import After5 from "../../assets/Gafter5.jpeg";
-import Halloween1 from "../../assets/Halloween1.jpeg";
-import Halloween2 from "../../assets/Halloween2.jpeg";
+import G6 from "../../assets/G6.jpeg";
+import G7 from "../../assets/G7.jpeg";
 
-import { AfterImg, BeforeImg, GalleryBackground, GalleryCard, GallerySubtitle, GalleryTitle } from "./styled";
+import {
+  AfterImg,
+  BeforeImg,
+  GalleryBackground,
+  GalleryCard,
+  GallerySubtitle,
+  GalleryTitle,
+} from "./styled";
 
 const Gallery = () => {
   return (
-      <GalleryBackground>
-        <Header />
-        <GalleryTitle>Gallery</GalleryTitle>
-        <GallerySubtitle>&quot;Stay Trippy, Little Yippy!&quot;</GallerySubtitle>
-        <GalleryCard>
-          <BeforeImg src={Halloween1} />
-          <AfterImg src={Halloween2} />
-        </GalleryCard>
-        <GalleryCard>
-          <BeforeImg src={Before4} />
-          <AfterImg src={After4} />
-        </GalleryCard>
-        <GalleryCard>
-          <BeforeImg src={Before3} />
-          <AfterImg src={After3} />
-        </GalleryCard>
-        <GalleryCard>
-          <BeforeImg src={Before2} />
-          <AfterImg src={After2} />
-        </GalleryCard>
-        <GalleryCard>
-          <BeforeImg src={Before1} />
-          <AfterImg src={After1} />
-        </GalleryCard>
-        <GalleryCard>
-          <BeforeImg src={Before5} />
-          <AfterImg src={After5} />
-        </GalleryCard>
-        <Footer />
-      </GalleryBackground>
+    <GalleryBackground>
+      <Header />
+      <GalleryTitle>Gallery</GalleryTitle>
+      <GallerySubtitle>&quot;Stay Trippy, Little Yippy!&quot;</GallerySubtitle>
+      <GalleryCard>
+        <BeforeImg src={Before1} />
+        <AfterImg src={After1} />
+      </GalleryCard>
+      <GalleryCard>
+        <BeforeImg src={Before2} />
+        <AfterImg src={After2} />
+      </GalleryCard>
+      <GalleryCard>
+        <BeforeImg src={Before3} />
+        <AfterImg src={After3} />
+      </GalleryCard>
+      <GalleryCard>
+        <BeforeImg src={G6} />
+        <AfterImg src={G7} />
+      </GalleryCard>
+      <GalleryCard>
+        <BeforeImg src={Before5} />
+        <AfterImg src={After5} />
+      </GalleryCard>
+      <GalleryCard>
+        <BeforeImg src={Before4} />
+        <AfterImg src={After4} />
+      </GalleryCard>
+      <Footer />
+    </GalleryBackground>
   );
 };
 
